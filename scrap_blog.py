ed3d70582b3ab3d17816eb990eab400e8644dcda
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-18 14:45:55 -0500

    Add html parser in code

--- scrap_blog.py
+++ scrap_blog.py
@@ -3,4 +3,6 @@ from bs4 import BeautifulSoup
 import csv
 
 response = requests.get("https://www.rithmschool.com/blog")
-print (response.text)
+soup = BeautifulSoup(response.text, "html.parser")
+articles = soup.find_all("article")
+print(articles)
