eedc2e4a6235aec8f2883fc706d7fa83071ad93d
Sarath Emuri <etsarathchowdary@outlook.com>
2022-02-18 14:56:39 -0500

    Add a for loop to scrap title, url and date of each article in the blog page

--- scrap_blog.py
+++ scrap_blog.py
@@ -5,4 +5,10 @@ import csv
 response = requests.get("https://www.rithmschool.com/blog")
 soup = BeautifulSoup(response.text, "html.parser")
 articles = soup.find_all("article")
-print(articles)
+
+
+for article in articles:
+    title = article.find("a").get_text()
+    url = article.find("a")["href"]
+    date = article.find("time")["datetime"]
+    print(title, url, date)
